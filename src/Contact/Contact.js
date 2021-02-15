f42a4fb5d8eb32f5b8868f526c71bf5d77dd19f5
santhosh.pemmaka <santhosh.pemmaka@gmail.com>
2021-02-15 12:04:20 +0530

    changes in the contact page

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,10 +1,35 @@
 import React from "react";
 import "./Contact.css";
-
+import image_profile from "../IMG_20210110_085933 (1).jpg";
 function Contact() {
   return (
     <div style={{ backgroundColor: "#474747" }} className="Contact_header">
-      <span>Contact</span>
+      <div className="Container">
+        <div className="Container_row">
+          <div className="column_row">
+            <div className="image_row">
+              <div className="image">
+                <img src={image_profile} alt="me" />
+              </div>
+            </div>
+          </div>
+          <div className="column_row">
+            <br />
+            <h1
+              style={{ color: "white", fontFamily: "Poppins, helvetica" }}
+              className="Contact"
+            >
+              Contact Me
+            </h1>
+                      <br />
+                      <a href="/">
+                          <button className="btn btn_secondary">
+                              Profile
+                          </button>
+                      </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
