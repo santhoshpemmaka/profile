f5fc38109a3a5e30f4a9f66bca518a78b2a39c3d
santhosh.pemmaka <santhosh.pemmaka@gmail.com>
2021-02-16 11:31:11 +0530

    Home page changes

---
+++ src/Container/Container.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Container.css";
+function Container() {
+  return (
+    <div className="Home">
+      <div className="Container">
+        <div className="Container_title">
+          <h1>Santhosh</h1>
+          <h6>I am a frontend developer!</h6>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Container;
