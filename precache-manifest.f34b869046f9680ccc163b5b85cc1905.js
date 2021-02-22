c468c624e4b7683bf0d66999a8e518364807788b
santhosh.pemmaka <santhosh.pemmaka@gmail.com>
2021-02-22 18:01:21 +0530

    Updates

---
+++ precache-manifest.f34b869046f9680ccc163b5b85cc1905.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed6f7ca1ad6be62ee098",
+    "url": "/profile/static/css/main.b1714806.chunk.css"
+  },
+  {
+    "revision": "ed6f7ca1ad6be62ee098",
+    "url": "/profile/static/js/main.ed6f7ca1.chunk.js"
+  },
+  {
+    "revision": "f272c805d18a86c6de70",
+    "url": "/profile/static/js/1.f272c805.chunk.js"
+  },
+  {
+    "revision": "92700b96a6acba4945f6",
+    "url": "/profile/static/js/runtime~main.92700b96.js"
+  },
+  {
+    "revision": "c908ce42967f49d526648b6ca6268f92",
+    "url": "/profile/static/media/contactPage.c908ce42.jpg"
+  },
+  {
+    "revision": "1db00d246f8355fe0a1e3949ad713f6e",
+    "url": "/profile/static/media/homePage.1db00d24.png"
+  },
+  {
+    "revision": "4443225b3a96d670f11c86ef75536081",
+    "url": "/profile/index.html"
+  }
+];
